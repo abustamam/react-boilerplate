be8632b371da1713404445bcc8e753565e7a14e1
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2016-07-03 06:32:49 -0700

    fix linting errors

--- libs/parts.js
+++ libs/parts.js
@@ -3,158 +3,158 @@ const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const PurifyCSSPlugin = require('purifycss-webpack-plugin')
 
-exports.devServer = function(options) {
-	return {
-		devServer: {
-			historyApiFallback: true,
-			hot: true,
-			inline: true,
-			stats: 'errors-only',
-			host: options.host,
-			port: options.port
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin({
-				multiStep: true
-			})
-		]
-	}
+exports.devServer = function (options) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      stats: 'errors-only',
+      host: options.host,
+      port: options.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({
+        multiStep: true
+      })
+    ]
+  }
 }
 
-exports.setupCSS = function(paths) {
-	return {
-		module: {
-			loaders: [
-				{
-			      test: /\.sass$/,
-			      loaders: ['style', 'css', 'sass'],
-			      include: paths
-			    },
-				{
-					test: /\.css$/,
-					loaders: ['style', 'css'],
-					include: paths
-				}
-			]
-		}
-	}
+exports.setupCSS = function (paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.sass$/,
+          loaders: ['style', 'css', 'sass'],
+          include: paths
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: paths
+        }
+      ]
+    }
+  }
 }
 
-exports.minify = function() {
-	return {
-		plugins: [
-			new webpack.optimize.UglifyJsPlugin({
-				compress: {
-					warnings: false
-				},
-				mangle: {
-					except: ['webpackJsonp']
-				}
-			})
-		]
-	}
+exports.minify = function () {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        mangle: {
+          except: ['webpackJsonp']
+        }
+      })
+    ]
+  }
 }
 
-exports.setFreeVariable = function(key, value) {
-	const env = {}
-	env[key] = JSON.stringify(value)
-	return {
-		plugins: [
-			new webpack.DefinePlugin(env)
-		]
-	}
+exports.setFreeVariable = function (key, value) {
+  const env = {}
+  env[key] = JSON.stringify(value)
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  }
 }
 
-exports.extractBundle = function(options) {
-	const entry = {}
-	entry[options.name] = options.entries
+exports.extractBundle = function (options) {
+  const entry = {}
+  entry[options.name] = options.entries
 
-	return {
-		entry,
-		plugins: [
-			new webpack.optimize.CommonsChunkPlugin({
-				names: [options.name, 'manifest']
-			})
-		]
-	}
+  return {
+    entry,
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        names: [options.name, 'manifest']
+      })
+    ]
+  }
 }
 
-exports.clean = function(path) {
-	return {
-		plugins: [
-			new CleanWebpackPlugin([path], {
-				root: process.cwd()
-			})
-		]
-	}
+exports.clean = function (path) {
+  return {
+    plugins: [
+      new CleanWebpackPlugin([path], {
+        root: process.cwd()
+      })
+    ]
+  }
 }
 
-exports.extractCSS = function(paths) {
-	return {
-		module: {
-			loaders: [
-				{
-					test: /\.sass$/,
-					loader: ExtractTextPlugin.extract('style', 'css!sass'),
-					include: paths
-				},
-				{
-					test: /\.css$/,
-					loader: ExtractTextPlugin.extract('style', 'css'),
-					include: paths
-				}
-			]
-		},
-		plugins: [
-			new ExtractTextPlugin('[name].[chunkhash].css')
-		]
-	}
+exports.extractCSS = function (paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.sass$/,
+          loader: ExtractTextPlugin.extract('style', 'css!sass'),
+          include: paths
+        },
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: paths
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].[chunkhash].css')
+    ]
+  }
 }
 
-exports.purifyCSS = function(paths) {
-	return {
-		plugins: [
-			new PurifyCSSPlugin({
-				basePath: process.cwd(),
-				paths
-			})
-		]
-	}
+exports.purifyCSS = function (paths) {
+  return {
+    plugins: [
+      new PurifyCSSPlugin({
+        basePath: process.cwd(),
+        paths
+      })
+    ]
+  }
 }
 
-exports.images = function(paths) {
-	return {
-		module: {
-			loaders: [
-				{
-					test: /\.(jpg|png)$/,
-					loaders: [
-						'file?name=[path][name].[hash].[ext]',
-						'url?limit=25000',
-						'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-					],
-					include: paths
-				}
-			]
-		}
-	}
+exports.images = function (paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.(jpg|png)$/,
+          loaders: [
+            'file?name=[path][name].[hash].[ext]',
+            'url?limit=25000',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ],
+          include: paths
+        }
+      ]
+    }
+  }
 }
 
-exports.fonts = function(paths) {
-	return {
-		module: {
-			loaders: [
-				{
-					test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-					loader: 'url',
-					query: {
-						limit: 50000,
-						mimetype: 'application/font-woff',
-						name: './fonts/[hash].[ext]'
-					},
-					include: paths
-				}
-			]
-		}
-	}
+exports.fonts = function (paths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url',
+          query: {
+            limit: 50000,
+            mimetype: 'application/font-woff',
+            name: './fonts/[hash].[ext]'
+          },
+          include: paths
+        }
+      ]
+    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,79 +8,88 @@ const pkg = require('./package.json')
 const parts = require('./libs/parts')
 
 const PATHS = {
-	app: path.join(__dirname, 'app'),
-	style: path.join(__dirname, 'app', 'stylesheets', 'main.sass'),
-	build: path.join(__dirname, 'build'),
-	images: path.join(__dirname, 'public', 'assets', 'images'),
-	fonts: path.join(__dirname, 'public', 'assets', 'fonts')
+  app: path.join(__dirname, 'app'),
+  style: path.join(__dirname, 'app', 'stylesheets', 'main.sass'),
+  build: path.join(__dirname, 'build'),
+  images: path.join(__dirname, 'public', 'assets', 'images'),
+  fonts: path.join(__dirname, 'public', 'assets', 'fonts')
 }
 
 const common = {
-	entry: {
-		style: PATHS.style,
-		app: PATHS.app
-	},
-	output: {
-		path: PATHS.build,
-		filename: '[name].js'
-	}, 
-	plugins: [
-		new FaviconsWebpackPlugin('./public/assets/images/webpack.png'),
-		new HtmlWebpackPlugin({
-			title: 'Webpack Demo'
-		})
-	]
+  entry: {
+    style: PATHS.style,
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new FaviconsWebpackPlugin('./public/assets/images/webpack.png'),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Demo'
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: PATHS.app
+      }
+    ]
+  }
 }
 
 let config
 let opts = {}
 
-switch(process.env.npm_lifecycle_event) {
-	case 'stats':
-		opts.quiet = true
-	case 'build': 
-		console.log("BUILD")
-		config = merge(
-			common,
-			{
-				devtool: 'source-map',
-				output: {
-					path: PATHS.build,
-					filename: '[name].[chunkhash].js',
-					chunkFilename: '[chunkhash].js'
-				}
-			},
-			parts.clean(PATHS.build),
-			parts.setFreeVariable(
-				'process.env.NODE_ENV',
-				'production'
-			),
-			parts.extractBundle({
-				name: 'vendor',
-				entries: Object.keys(pkg.dependencies)
-			}),
-			parts.minify(),
-			parts.images(PATHS.images),
-			parts.fonts(PATHS.fonts),
-			parts.extractCSS(PATHS.style),
-			parts.purifyCSS([PATHS.app])
-		)
-		break
-	default:
-		console.log("DEFAULT")
-		config = merge(
-			common, 
-			{
-				devtool: 'eval-source-map'
-			},
-			parts.devServer({
-				host: process.env.HOST,
-				port: process.env.PORT
-			}),
-			parts.images(PATHS.images),
-			parts.fonts(PATHS.fonts),
-			parts.setupCSS(PATHS.style)
-		)
+switch (process.env.npm_lifecycle_event) {
+case 'stats':
+  opts.quiet = true
+case 'build':
+  console.log('BUILD')
+  config = merge(
+    common,
+    {
+      devtool: 'source-map',
+      output: {
+        path: PATHS.build,
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[chunkhash].js'
+      }
+    },
+    parts.clean(PATHS.build),
+    parts.setFreeVariable(
+      'process.env.NODE_ENV',
+      'production'
+    ),
+    parts.extractBundle({
+      name: 'vendor',
+      entries: Object.keys(pkg.dependencies)
+    }),
+    parts.minify(),
+    parts.images(PATHS.images),
+    parts.fonts(PATHS.fonts),
+    parts.extractCSS(PATHS.style),
+    parts.purifyCSS([PATHS.app])
+  )
+  break
+default:
+  console.log('DEFAULT')
+  config = merge(
+    common,
+    {
+      devtool: 'eval-source-map'
+    },
+    parts.devServer({
+      host: process.env.HOST,
+      port: process.env.PORT
+    }),
+    parts.images(PATHS.images),
+    parts.fonts(PATHS.fonts),
+    parts.setupCSS(PATHS.style)
+  )
 }
 
 module.exports = validate(config, opts)
