ea45b177230ad29b010bfd17bbd02dc5583c5d0c
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2016-07-02 20:05:34 -0700

    initial commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+const component = require('./component')
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './components/app'
+
+// require('./stylesheets/main.sass')
+
+// ReactDOM.render(<App />, document.getElementById('root'))
+
+document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,22 @@
 const path = require('path')
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const NpmInstallPlugin = require('npm-install-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const validate = require('webpack-validator')
 
-const TARGET = process.env.npm_lifecyle_event
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
 }
 
-const common = {
-  devtool: 'eval',
-  entry: {
-    app: PATHS.app
-  },
-  output: {
-    path: PATHS.build,
-    filename: 'bundle.js',
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [{
-      test: /\.js|\.jsx$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    },
-    {
-      test: /\.sass$/,
-      loader: "style!css!sass"
-    },
-    {
-      test: /\.css$/,
-      loaders: "style!css"
-    }]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo'
-    }),
-    new FaviconsWebpackPlugin('../public/webpack.svg')
-  ]
-};
-
-let config
-
-switch(process.env.npm_lifecycle_event) {
-  case 'build':
-    config = merge(common, {
-
-    })
-    break
-  case 'start':
-    config = merge(common, {
-      devtool: 'eval-source-map',
-      devServer: {
-        contentBase: PATHS.build,
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-        stats: 'errors-only',
-        host: process.env.HOST,
-        port: process.env.PORT
-      },
-      plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new NpmInstallPlugin({save: true}),
-        new FaviconsWebpackPlugin('../public/webpack.svg')
-      ]
-    })
-    break
-  default:
-    config = merge(common, {})
+module.exports = {
+	entry: {
+		app: PATHS.app
+	},
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js'
+	}, 
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Webpack Demo'
+		})
+	]
 }
-
-module.exports = validate(config)
